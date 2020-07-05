5a0011b267101623a6efe22eecc960b1f6f924b5
Fawad <fawad_12@outlook.com>
2020-07-06 04:33:07 +0500

    customizing swagger content

--- src/main/java/com/wulf/systems/mes/MesApplication.java
+++ src/main/java/com/wulf/systems/mes/MesApplication.java
@@ -2,7 +2,15 @@ package com.wulf.systems.mes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -12,4 +20,25 @@ public class MesApplication {
         SpringApplication.run(MesApplication.class, args);
     }
 
+    @Bean
+    public Docket swaggerConfigurations(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.wulf.systems.mes"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+    private ApiInfo apiDetails(){
+        return new ApiInfo(
+                "Mes API",
+                "Products/Order API",
+                "1.0",
+                "Under Development",
+                new springfox.documentation.service.Contact("Fawad Bin Tariq","https://fawad.netlify.app/","fawad_12@outlook.com"),
+                "API Liscence",
+                "https://fawad.netlify.app/",
+                Collections.emptyList()
+        );
+    }
 }
